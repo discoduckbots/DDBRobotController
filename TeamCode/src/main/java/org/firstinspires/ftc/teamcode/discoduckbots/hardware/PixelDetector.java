af538b9edb82ba0d8d8ee8516f0b2e33510ccd74
Dustin Good <dustingood@gmail.com>
2024-02-12 17:43:16 -0600

    updates from autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/discoduckbots/hardware/PixelDetector.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.discoduckbots.hardware;
+
+import android.graphics.Color;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class PixelDetector {
+
+    private NormalizedColorSensor leftColorSensor;
+    private NormalizedColorSensor rightColorSensor;
+    private RevBlinkinLedDriver lights;
+
+    private static final int RANGE = 40; //mm of range
+
+    public PixelDetector(NormalizedColorSensor leftColorSensor, NormalizedColorSensor rightColorSensor, RevBlinkinLedDriver lights){
+        this.leftColorSensor = leftColorSensor;
+        this.rightColorSensor = rightColorSensor;
+        this.lights = lights;
+        lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+        /*lights = hardwareMap.get(RevBlinkinLedDriver.class, "lights");*/
+    }
+
+    public RevBlinkinLedDriver getLights(){
+        return lights;
+    }
+
+    private double getDistance (NormalizedColorSensor colorSensor){
+        DistanceSensor distanceSensor = (DistanceSensor) colorSensor;
+        return distanceSensor.getDistance(DistanceUnit.MM);
+    }
+
+    private void detectColors(){
+        final float[] leftHsvValues = new float[3];
+        final float[] rightHsvValues = new float[3];
+
+        NormalizedRGBA leftColors = leftColorSensor.getNormalizedColors();
+        Color.colorToHSV(leftColors.toColor(), leftHsvValues);
+
+        NormalizedRGBA rightColors = rightColorSensor.getNormalizedColors();
+        Color.colorToHSV(rightColors.toColor(), rightHsvValues);
+    }
+
+    public boolean isBothPixels(){
+        return isLeftPixel() && isRightPixel();
+    }
+
+    public boolean isLeftPixel(){
+        return getDistance(leftColorSensor) < RANGE;
+    }
+
+    public boolean isRightPixel(){
+        return getDistance(rightColorSensor) < RANGE;
+    }
+
+
+}
